b756dba8f2881d24e2c87e95df78f8642e924e7b
Nathan de assis Crevelin <neitass.music@gmail.com>
2023-03-28 17:10:58 -0300

    termina 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+it('testa se exibe nenhum pokemon favoritado', async () => {
+  // renderWithRouter(<App />);
+  const { history } = renderWithRouter(<App />);
+  const favoritePokemon = screen.getByRole('link', { name: 'Favorite Pokémon' });
+  userEvent.click(favoritePokemon);
+
+  const { pathname } = history.location;
+  expect(pathname).toBe('/favorites');
+
+  const aboutTitleOne = await screen.findByText('No favorite Pokémon found');
+  expect(aboutTitleOne).toBeInTheDocument();
+});
+
+it('testa se exibe adiciona pokemons', async () => {
+  // renderWithRouter(<App />);
+  renderWithRouter(<App />);
+
+  const details = screen.getByRole('link', { name: 'More details' });
+  userEvent.click(details);
+
+  const favoriteLabel = await screen.findByText('Pokémon favoritado?');
+  userEvent.click(favoriteLabel);
+
+  const favoritePokemon = await screen.findByRole('link', { name: 'Favorite Pokémon' });
+  userEvent.click(favoritePokemon);
+
+  const aboutTitleOne = await screen.findByText('Pikachu');
+  expect(aboutTitleOne).toBeInTheDocument();
+});
