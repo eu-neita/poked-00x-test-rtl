de91ed654c1f48ff40ad670ee190a86f8c730a59
Nathan de assis Crevelin <neitass.music@gmail.com>
2023-03-28 15:35:33 -0300

    termina requisito 1

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+// test('', () => {});
+it('deve renderizar o os links no componente app', () => {
+  renderWithRouter(<App />);
+
+  const homeLink = screen.getByRole('link', { name: 'Home' });
+  expect(homeLink).toBeInTheDocument();
+
+  const aboutLink = screen.getByRole('link', { name: 'About' });
+  expect(aboutLink).toBeInTheDocument();
+
+  const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémon' });
+  expect(favoriteLink).toBeInTheDocument();
+});
