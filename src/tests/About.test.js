43926c2c10c1e592c40ef3b8f2c5299e3c17ff6d
Nathan de assis Crevelin <neitass.music@gmail.com>
2023-03-28 15:56:09 -0300

    termina requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-// test('', () => {});
-it('deve renderizar o os links no componente app', () => {
-  renderWithRouter(<App />);
-
-  const homeLink = screen.getByRole('link', { name: 'Home' });
-  expect(homeLink).toBeInTheDocument();
-
+it('testa se exibe informações sobre a pokedex', () => {
+  // renderWithRouter(<App />);
+  const { history } = renderWithRouter(<App />);
   const aboutLink = screen.getByRole('link', { name: 'About' });
   expect(aboutLink).toBeInTheDocument();
+  userEvent.click(aboutLink);
 
-  const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémon' });
-  expect(favoriteLink).toBeInTheDocument();
+  const { pathname } = history.location;
+  expect(pathname).toBe('/about');
+
+  const aboutTitle = screen.getByRole(
+    'heading',
+    { name: 'About Pokédex', level: 2 },
+  );
+  expect(aboutTitle).toBeInTheDocument();
+
+  const pPokedex = screen.getByText(/This application simulates a Pokédex/i);
+  expect(pPokedex).toBeInTheDocument();
+
+  const pPokedexTwo = screen.getByText(/One can filter Pokémon by type,/i);
+  expect(pPokedexTwo).toBeInTheDocument();
 });
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+// test('', () => {});
+it('deve renderizar o os links no componente app', () => {
+  renderWithRouter(<App />);
+
+  const homeLink = screen.getByRole('link', { name: 'Home' });
+  expect(homeLink).toBeInTheDocument();
+
+  const aboutLink = screen.getByRole('link', { name: 'About' });
+  expect(aboutLink).toBeInTheDocument();
+
+  const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémon' });
+  expect(favoriteLink).toBeInTheDocument();
+
+  const imagePokedex = screen.getByRole('img', { src: 'Pokédex' });
+  expect(imagePokedex).toBeInTheDocument();
+});
